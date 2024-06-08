bcaa1f878aad1acdf8adfcb8b2a6186b1955bd92
Nanang-Wahyudi <nanang.wahyudi220503@gmail.com>
2024-06-08 10:33:43 +0700

    add: init project

---
+++ Jobsite_ServerApp/src/main/java/sib6/finalproject/Jobsite_ServerApp/JobsiteServerAppApplication.java
@@ -0,0 +1,13 @@
+package sib6.finalproject.Jobsite_ServerApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobsiteServerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobsiteServerAppApplication.class, args);
+	}
+
+}
---
+++ Jobsite_ServerApp/src/test/java/sib6/finalproject/Jobsite_ServerApp/JobsiteServerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package sib6.finalproject.Jobsite_ServerApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobsiteServerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
