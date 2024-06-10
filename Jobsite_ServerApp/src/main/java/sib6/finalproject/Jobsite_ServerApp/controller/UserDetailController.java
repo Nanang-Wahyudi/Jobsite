41be80fab749e390237ff0d5c02a38dec70d24a1
Nanang-Wahyudi <nanang.wahyudi220503@gmail.com>
2024-06-10 20:26:42 +0700

    bug-fix: batasi ketika user upload file selain gambar pada update user detail

--- Jobsite_ServerApp/src/main/java/sib6/finalproject/Jobsite_ServerApp/controller/UserDetailController.java
+++ Jobsite_ServerApp/src/main/java/sib6/finalproject/Jobsite_ServerApp/controller/UserDetailController.java
@@ -11,7 +11,6 @@ import sib6.finalproject.Jobsite_ServerApp.service.UserDetailService;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
-import java.io.IOException;
 import java.util.Date;
 
 @RequestMapping("/api/user")
@@ -22,7 +21,7 @@ public class UserDetailController {
     private UserDetailService userDetailService;
 
     @PutMapping("/update/{username}")
-    public ResponseEntity<?> updateUserDetail(HttpServletRequest servletRequest, @PathVariable("username") String username, @Valid @ModelAttribute UpdateUserDetailRequest userDetailRequest, BindingResult result) throws IOException {
+    public ResponseEntity<?> updateUserDetail(HttpServletRequest servletRequest, @PathVariable("username") String username, @Valid @ModelAttribute UpdateUserDetailRequest userDetailRequest, BindingResult result) {
         Response response = Response.builder()
                 .url(servletRequest.getRequestURL().toString())
                 .status(HttpStatus.OK.toString())
--- Jobsite_ServerApp/src/main/java/sib6/finalproject/Jobsite_ServerApp/service/UserDetailService.java
+++ Jobsite_ServerApp/src/main/java/sib6/finalproject/Jobsite_ServerApp/service/UserDetailService.java
@@ -2,10 +2,8 @@ package sib6.finalproject.Jobsite_ServerApp.service;
 
 import sib6.finalproject.Jobsite_ServerApp.model.request.UpdateUserDetailRequest;
 
-import java.io.IOException;
-
 public interface UserDetailService {
 
-    String updateUserDetail(String username, UpdateUserDetailRequest userDetailRequest) throws IOException;
+    String updateUserDetail(String username, UpdateUserDetailRequest userDetailRequest);
 
 }
