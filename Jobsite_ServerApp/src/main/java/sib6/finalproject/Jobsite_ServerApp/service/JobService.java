4106470e242851da698b3e45bc41a7d166fe0019
khaidar-ar <96arrijal@gmail.com>
2024-06-10 21:11:15 +0700

    add : update,delete job

--- Jobsite_ServerApp/src/main/java/sib6/finalproject/Jobsite_ServerApp/service/JobService.java
+++ Jobsite_ServerApp/src/main/java/sib6/finalproject/Jobsite_ServerApp/service/JobService.java
@@ -1,9 +1,16 @@
 package sib6.finalproject.Jobsite_ServerApp.service;
 
+import sib6.finalproject.Jobsite_ServerApp.entity.Job;
 import sib6.finalproject.Jobsite_ServerApp.model.request.CreateJobRequest;
+import sib6.finalproject.Jobsite_ServerApp.model.request.UpdateJobRequest;
 
 public interface JobService {
 
+    Job findById(String id);
+
     String createJob(CreateJobRequest jobRequest);
 
+    String updateJob(UpdateJobRequest updateJobRequest,String id);
+
+    String delete(String id);
 }
--- Jobsite_ServerApp/src/main/java/sib6/finalproject/Jobsite_ServerApp/service/impl/JobServiceImpl.java
+++ Jobsite_ServerApp/src/main/java/sib6/finalproject/Jobsite_ServerApp/service/impl/JobServiceImpl.java
@@ -10,12 +10,14 @@ import sib6.finalproject.Jobsite_ServerApp.entity.Job;
 import sib6.finalproject.Jobsite_ServerApp.entity.User;
 import sib6.finalproject.Jobsite_ServerApp.model.enums.JobTypeEnum;
 import sib6.finalproject.Jobsite_ServerApp.model.request.CreateJobRequest;
+import sib6.finalproject.Jobsite_ServerApp.model.request.UpdateJobRequest;
 import sib6.finalproject.Jobsite_ServerApp.repository.CompanyRepository;
 import sib6.finalproject.Jobsite_ServerApp.repository.JobRepository;
 import sib6.finalproject.Jobsite_ServerApp.repository.UserRepository;
 import sib6.finalproject.Jobsite_ServerApp.service.JobService;
 
 import java.util.Date;
+import java.util.Optional;
 
 @Service
 public class JobServiceImpl implements JobService {
@@ -31,6 +33,17 @@ public class JobServiceImpl implements JobService {
 
     @Autowired
     private ModelMapper modelMapper;
+
+    @Override
+    @SuppressWarnings("null")
+    public Job findById(String id) {
+        Job result = jobRepository.findById(id).orElseThrow(() ->
+                new ResponseStatusException(
+                        HttpStatus.NOT_FOUND,
+                        "Job is not found!!!"
+                ));
+        return result;
+    }
 
     @Override
     public String createJob(CreateJobRequest jobRequest) {
@@ -49,6 +62,32 @@ public class JobServiceImpl implements JobService {
         jobRepository.save(job);
 
         return "Create Successfully with Job Title: " + jobRequest.getTitle();
+    }
+
+    @Override
+    public String updateJob(UpdateJobRequest updateJobRequest,String id) {
+        this.findById(id);
+        Job job = modelMapper.map(updateJobRequest, Job.class);
+
+        User user = userRepository.findByUsername(updateJobRequest.getUsername())
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+                        "Not Found Username: " + updateJobRequest.getUsername()));
+        Company company = companyRepository.findById(user.getCompany().getId())
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Company Not Found with " +
+                        "Username: " + updateJobRequest.getUsername()));
+        job.setType(JobTypeEnum.valueOf(updateJobRequest.getType().toUpperCase()));
+        job.setCompany(company);
+        job.setIsactive(Boolean.TRUE);
+        job.setPostDate(new Date());
+        jobRepository.save(job);
+        return "Update Successfully with Job Title: "+updateJobRequest.getTitle();
+    }
+
+    @Override
+    public String delete(String id) {
+        Job job = this.findById(id);
+        jobRepository.delete(job);
+        return "Delete Successfully with Job Title: "+job.getTitle();
     }
 
 }
