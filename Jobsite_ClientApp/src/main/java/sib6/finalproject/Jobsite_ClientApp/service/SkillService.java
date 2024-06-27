b75bc0d780a3a1ae9176ccab0a5d0bd27cd9992b
Romi <romiputrapamungkas@gmail.com>
2024-06-27 15:32:36 +0700

    add: Integrasi delete skill pada profile talent

---
+++ Jobsite_ClientApp/src/main/java/sib6/finalproject/Jobsite_ClientApp/service/SkillService.java
@@ -0,0 +1,9 @@
+package sib6.finalproject.Jobsite_ClientApp.service;
+
+import sib6.finalproject.Jobsite_ClientApp.model.response.SkillResponse;
+
+public interface SkillService {
+    
+    SkillResponse deleteSkill(String id);
+
+}
